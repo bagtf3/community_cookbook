148d83e3e54a28bfdcf6f51e53106197b1b7b31c
Bryan Goggin <bgoggin88@gmail.com>
2021-04-11 11:20:44 -0500

    templates

--- app.py
+++ app.py
@@ -26,12 +26,46 @@ app.config['SECRET_KEY'] = 'your secret key'
 
 @app.route('/')
 def index():
-    conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
-    conn.close()
-    return render_template('index.html', posts=posts)
+    #conn = get_db_connection()
+    #posts = conn.execute('SELECT * FROM posts').fetchall()
+    #conn.close()
+    return render_template('index.html')
 
+@app.route('/search', methods=('GET', 'POST'))
+def search():
+    if request.method == 'POST':
+        # title = request.form['title']
+        # content = request.form['content']
 
+        # if not title:
+        #     flash('Title is required!')
+        # else:
+        #     conn = get_db_connection()
+        #     conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+        #                  (title, content))
+        #     conn.commit()
+        #     conn.close()
+        return redirect(url_for('index'))
+
+    return render_template('search.html')
+
+@app.route('/upload', methods=('GET', 'POST'))
+def upload():
+    if request.method == 'POST':
+        # title = request.form['title']
+        # content = request.form['content']
+
+        # if not title:
+        #     flash('Title is required!')
+        # else:
+        #     conn = get_db_connection()
+        #     conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)',
+        #                  (title, content))
+        #     conn.commit()
+        #     conn.close()
+        return redirect(url_for('index'))
+
+    return render_template('upload.html')
 
 @app.route('/<int:post_id>')
 def post(post_id):
